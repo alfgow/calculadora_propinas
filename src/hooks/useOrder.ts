3760a0d88196c85b4c5476d551343c335b0b0dbc
alfgow <alfonso@villanuevagarcia.com>
2024-04-26 01:31:38 -0600

    118: Mostrando Consumo

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -2,13 +2,33 @@ import { useState } from "react";
 import type { MenuItem, OrderItem } from "../types";
 
 export default function useOrder() {
-    const [order, setOrder] = useState<OrderItem[]>([])
+	const [order, setOrder] = useState<OrderItem[]>([]);
 
-    const addItem = (item: MenuItem) => {
-        console.log(item);
-    } 
+	const addItem = (item: MenuItem) => {
+		const itemExists = order.find(
+			(orderItem) => orderItem.id === item.id
+		);
 
-    return {
-        addItem
-    }
+		if (itemExists) {
+			const updatedOrder = order.map((orderItem) =>
+				orderItem.id === item.id
+					? {
+							...orderItem,
+							quantity:
+								orderItem.quantity +
+								1,
+					  }
+					: orderItem
+			);
+			setOrder(updatedOrder);
+		} else {
+			const newItem: OrderItem = { ...item, quantity: 1 };
+			setOrder([...order, newItem]);
+		}
+	};
+
+	return {
+		order,
+		addItem,
+	};
 }
