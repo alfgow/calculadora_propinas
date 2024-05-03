c885ba14745f453ee65abed5bd4a3b51afe24346
alfonsovillanueva <alfonso@villanuevagarcia.com>
2024-05-03 13:20:04 -0600

    128: Reiniciando Datos

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -12,12 +12,7 @@ function OrderContents({ order, removeItem }: OrderContentProps) {
 		<div>
 			<h2 className="font-black text-4xl text-center">Consumo</h2>
 			<div className="space-y-3 mt-10">
-				{order.length === 0 ? (
-					<p className="text-center">
-						La Orden está vacía
-					</p>
-				) : (
-					order.map((item) => (
+				{order.map((item) => (
 						<div key={item.id} className="flex justify-between items-center border-t border-gray-200 py-5 last-of-type:border-b">
                             <div>
 							<p className="text-lg">
@@ -32,7 +27,7 @@ function OrderContents({ order, removeItem }: OrderContentProps) {
                             </button>
 						</div>
 					))
-				)}
+				}
 			</div>
 		</div>
 	);
--- src/components/TipPercentageForm.tsx
+++ src/components/TipPercentageForm.tsx
@@ -2,22 +2,24 @@ import { tipOptions } from "../data/tips"
 
 type TipPercentageFormProps  = {
     setTip: React.Dispatch<React.SetStateAction<number>>
+    tip: number
 }
 
-export default function TipPercentageForm({setTip}: TipPercentageFormProps) {
+export default function TipPercentageForm({setTip, tip}: TipPercentageFormProps) {
   return (
     <div>
         <h3 className="font-black text-2xl">Propina:</h3>
         <form action="">
-            {tipOptions.map(tip=>(
-                <div key={tip.id} className="flex gap-2">
-                    <label htmlFor={tip.id}>{tip.label}</label>
+            {tipOptions.map(tipOption=>(
+                <div key={tipOption.id} className="flex gap-2">
+                    <label htmlFor={tipOption.id}>{tipOption.label}</label>
                     <input 
-                        id={tip.id} 
+                        id={tipOption.id} 
                         type="radio" 
-                        name="tip" 
-                        value={tip.value}
+                        name="tipOption" 
+                        value={tipOption.value}
                         onChange={e => setTip(+e.target.value)}
+                        checked={tipOption.value === tip}
                     />
                  </div>
             ))}
